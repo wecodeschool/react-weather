a3ce9b1a3e7585570a453e3678f7aef017643255
Matt Dellac <matt@dellac.org>
2018-11-22 15:13:21 +0000

    add weather data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.city = "Lisbon";
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Welcome to the Weather app</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="weather-summary">
+        <div className="weather-summary-header">
+          <h1 id="weather-location">{this.city}</h1>
+          <div id="weather-date" className="weather-detail__text">
+            Thu 15:09
+          </div>
+          <div className="weather-detail__text" id="weather-description">
+            Clouds
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-6">
+            <div className="clearfix">
+              <img
+                className="weather__icon weather__icon--today"
+                src="http://openweathermap.org/img/w/02d.png"
+              />
+              <div className="weather-temp weather-temp--today">12</div>
+              <div className="weather-unit__text weather-unit__text--today">
+                °C
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="weather-detail__text">
+              Precipitation: <span id="precipitation-probality">62%</span>
+            </div>
+            <div className="weather-detail__text">
+              Wind: <span id="wind-speed">4 km/h</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
-export default App;
