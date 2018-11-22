76af89988a998529d76afe93898ff2d7b6cbecf0
Matt Dellac <matt@dellac.org>
2018-11-22 12:25:58 +0000

    Cleanup

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Welcome to the Weather app</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
