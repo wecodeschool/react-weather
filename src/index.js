085082ba31417d93a8d341e65ff6325ed981d8a8
Matt Dellac <matt@dellac.org>
2018-11-29 17:11:11 +0000

    add props

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 
-ReactDom.render(<App />, document.querySelector("#weather"));
+ReactDom.render(<App />, document.querySelector("#lisbon"));
+ReactDom.render(<App city="Sydney" />, document.querySelector("#sydney"));
